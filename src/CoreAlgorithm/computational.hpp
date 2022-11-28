280cb89646743ef23b3cfbf43b1768c24c3826c9
Yan Cheng <yc3855@columbia.edu>
2022-11-28 16:03:41 -0500

    Add files via upload

--- src/CoreAlgorithm/computational.hpp
+++ src/CoreAlgorithm/computational.hpp
@@ -5,6 +5,9 @@
 #ifndef WAVESIMC_COMPUTATIONAL_HPP
 #define WAVESIMC_COMPUTATIONAL_HPP
 
-boost::multi_array<double, 2> velocity()
+boost::multi_array<double, 2> get_profile()
+{
+
+}
 
 #endif //WAVESIMC_COMPUTATIONAL_HPP
--- src/CoreAlgorithm/helper_func.hpp
+++ src/CoreAlgorithm/helper_func.hpp
@@ -5,31 +5,42 @@
 #ifndef WAVESIMC_HELPER_FUNC_HPP
 #define WAVESIMC_HELPER_FUNC_HPP
 
-// Helper functions
-boost::multi_array<double, 2> d2fdx2(boost::multi_array<double, 2> f, double dx)
-{
-    return f;
-}
-
-boost::multi_array<double, 2> d2fdz2(boost::multi_array<double, 2> f, double dz)
-{
-    return f;
-}
+#include "CustomLibraries/np.hpp"
 
 boost::multi_array<double, 2> dfdx(boost::multi_array<double, 2> f, double dx)
 {
-    return f;
+    std::vector<boost::multi_array<double, 2>> grad_f = np::gradient(f, {dx, dx});
+    return grad_f[0];
 }
 
 boost::multi_array<double, 2> dfdz(boost::multi_array<double, 2> f, double dz)
 {
-    return f;
+    std::vector<boost::multi_array<double, 2>> grad_f = np::gradient(f, {dz, dz});
+    return grad_f[1];
+}
+
+boost::multi_array<double, 2> d2fdx2(boost::multi_array<double, 2> f, double dx)
+{
+    boost::multi_array<double, 2> f_x = dfdx(f, dx);
+    boost::multi_array<double, 2> f_xx = dfdx(f_x, dx);
+    return f_xx;
+}
+
+boost::multi_array<double, 2> d2fdz2(boost::multi_array<double, 2> f, double dz)
+{
+    boost::multi_array<double, 2> f_z = dfdz(f, dz);
+    boost::multi_array<double, 2> f_zz = dfdx(f_z, dz);
+    return f_zz;
 }
 
 boost::multi_array<double, 2> divergence(boost::multi_array<double, 2> f1, boost::multi_array<double, 2> f2,
                                          double dx, double dz)
 {
-    return f1;
+    boost::multi_array<double, 2> f_x = dfdx(f1, dx);
+    boost::multi_array<double, 2> f_z = dfdz(f2, dz);
+    // TODO: use element-wize add
+    div = f1 + f2;
+    return div;
 }
 
 
