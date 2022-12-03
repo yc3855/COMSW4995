6ca2d25b41000f53fc96fb549436a3a3b988c18c
Yan Cheng <yc3855@columbia.edu>
2022-12-02 22:12:26 -0500

    Add files via upload

--- src/CoreAlgorithm/computational.hpp
+++ src/CoreAlgorithm/computational.hpp
@@ -5,9 +5,32 @@
 #ifndef WAVESIMC_COMPUTATIONAL_HPP
 #define WAVESIMC_COMPUTATIONAL_HPP
 
-boost::multi_array<double, 2> get_profile()
+boost::multi_array<double, 2> get_profile(double xmin, double xmax, double zmin, double zmax, int nx, int nz)
 {
+    boost::multi_array<double, 2> c(boost::extents[nx][nz]);
 
+    boost::multi_array<double, 1> x = np::linspace(xmin, xmax, nx);
+    boost::multi_array<double, 1> z = np::linspace(zmin, zmax, nz);
+
+    const boost::multi_array<double, 1> axis[2] = {x, z};
+    std::vector<boost::multi_array<double, 2>> XZ = np::meshgrid(axis, false, np::xy);
+
+    double x_0 = xmax / 2.0;
+    double z_0 = zmax / 2.0;
+    double r = 0.2;
+
+    for (int i = 0; i < nx; i++)
+    {
+        for (int j = 0; j < nz; j++)
+        {
+            if (np::pow(XZ[0][i][j]-x_0, 2.0) + np::pow(XZ[1][i][j]-z_0, 2.0) <= np::pow(r, 2.0))
+                c[i][j] = 3000;
+            else
+                c[i][j] = 2500;
+        }
+    }
+
+    return c;
 }
 
 #endif //WAVESIMC_COMPUTATIONAL_HPP
