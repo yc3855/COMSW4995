a788260462950e20696decf9126895b5324c1048
Yan Cheng <yc3855@columbia.edu>
2022-12-06 19:15:55 -0500

    Add files via upload

--- src/CoreAlgorithm/solver.hpp
+++ src/CoreAlgorithm/solver.hpp
@@ -15,14 +15,14 @@ boost::multi_array<double, 3> wave_solver(boost::multi_array<double, 2> c,
                                           boost::multi_array<double, 3> f,
                                           boost::multi_array<double, 2> sigma_1, boost::multi_array<double, 2> sigma_2) {
 
-    const boost::multi_array<double, 2> C1 = 1.0 + dt * (sigma_1 + sigma_2) * 0.5;
-    const boost::multi_array<double, 2> C2 = sigma_1 * sigma_2 * np::pow(dt, 2.0) - 2.0;
-    const boost::multi_array<double, 2> C3 = 1.0 - dt * (sigma_1 + sigma_2) * 0.5;
+    const boost::multi_array<double, 2> C1 = 1.0 + (dt * (sigma_1 + sigma_2) * 0.5);
+    const boost::multi_array<double, 2> C2 = (sigma_1 * sigma_2 * np::pow(dt, 2.0)) - 2.0;
+    const boost::multi_array<double, 2> C3 = 1.0 - (dt * (sigma_1 + sigma_2) * 0.5);
     const boost::multi_array<double, 2> C4 = np::pow(dt * c, 2.0);
-    const boost::multi_array<double, 2> C5 = 1.0 + dt * sigma_1 * 0.5;
-    const boost::multi_array<double, 2> C6 = 1.0 + dt * sigma_2 * 0.5;
-    const boost::multi_array<double, 2> C7 = 1.0 - dt * sigma_1 * 0.5;
-    const boost::multi_array<double, 2> C8 = 1.0 - dt * sigma_2 * 0.5;
+    const boost::multi_array<double, 2> C5 = 1.0 + (dt * sigma_1 * 0.5);
+    const boost::multi_array<double, 2> C6 = 1.0 + (dt * sigma_2 * 0.5);
+    const boost::multi_array<double, 2> C7 = 1.0 - (dt * sigma_1 * 0.5);
+    const boost::multi_array<double, 2> C8 = 1.0 - (dt * sigma_2 * 0.5);
 
 
 
@@ -38,15 +38,6 @@ boost::multi_array<double, 3> wave_solver(boost::multi_array<double, 2> c,
     boost::multi_array<double, 2> u_xx = np::zeros<double>(dimensions_4);
     int dimensions_5[] = {nx, nz};
     boost::multi_array<double, 2> u_zz = np::zeros<double>(dimensions_5);
-
-    for (int i = 0; i < nx; i++)
-    {
-        for (int j = 0; j < nz; j++)
-        {
-            q_1[i][j] = 0.0;
-            q_2[i][j] = 0.0;
-        }
-    }
 
     boost::multi_array<double, 2> f_n(boost::extents[nx][nz]);
     boost::multi_array<double, 2> u_n(boost::extents[nx][nz]);
@@ -70,7 +61,19 @@ boost::multi_array<double, 3> wave_solver(boost::multi_array<double, 2> c,
         boost::multi_array<double, 2> dq_2dz = dfdz(q_2, dz);
         u_xx = d2fdx2(u_n, dx); // (nx, nz)
         u_zz = d2fdz2(u_n, dz); // (nx, nz)
+        boost::multi_array<double, 2> dudx = dfdx(u_n, dx);
+        boost::multi_array<double, 2> dudz = dfdz(u_n, dz);
 
+//        for (int i = 1; i < nx-1; i++)
+//        {
+//            for (int j = 1; j < nz-1; j++)
+//            {
+//                u_xx[i][j] = u_n[i+1][j] + u_n[i-1][j] - 2.0 * u_n[i][j];
+//                u_zz[i][j] = u_n[i][j+1] + u_n[i][j-1] - 2.0 * u_n[i][j];
+//            }
+//        }
+
+        // ! Update u
         for (int i = 0; i < nx; i++)
         {
             for (int j = 0; j < nz; j++)
@@ -82,9 +85,7 @@ boost::multi_array<double, 3> wave_solver(boost::multi_array<double, 2> c,
             }
         }
 
-        boost::multi_array<double, 2> dudx = dfdx(u_n, dx);
-        boost::multi_array<double, 2> dudz = dfdx(u_n, dz);
-
+        // ! Update q_1, q_2
         for (int i = 0; i < nx; i++)
         {
             for (int j = 0; j < nz; j++)
